de39ff5543ff60cd97741ca88686893d14870187
Brian Buchalter <brian.buchalter@shopify.com>
2018-05-12 15:46:26 -0400

    Chapters 1,2
    * Installing ROM
    * Connecting a Rails controller with ROM

---
+++ spec/features/projects/creating_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.feature "Users can create new projects" do
+  scenario "with valid attributes" do
+    visit "/"
+    click_link "New Project"
+    fill_in "Name", with: "Shopify"
+    click_button "Create Project"
+    expect(page).to have_content "Project has been created."
+  end
+end
